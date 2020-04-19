7fc5a0d65f4fb587e0a3fed3cd8df6e40f5a1f80
supaFool <supafool@live.com>
2020-04-19 13:51:56 -0400

    Adding Map Interface

---
+++ Assets/Scripts/Entity/MapBuilder/IMap.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IMap
+{
+    void Simulate(int[] samples);
+}
---
+++ Assets/Scripts/Entity/MapBuilder/MapFactory.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapFactory : IMap
+{
+    private MapSettings settings;
+
+    public MapFactory()
+    {
+        settings = new MapSettings();
+    }
+
+    public void Simulate(int[] samples)
+    {
+        throw new System.NotImplementedException();
+    }
+}
